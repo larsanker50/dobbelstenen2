8281bf70e68dafe494ef084f1e622f346d19351d
larsanker50 <75078152+larsanker50@users.noreply.github.com>
2021-01-04 15:45:38 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,20 +9,28 @@ const worp = {
     6: 0
 }
 
-
-
-
-
 button.onclick = function gooi() {
     console.log("er is gegooid");
     dobbel();
     console.log(dobbelArray);
-    console.log(worp)
+    console.log(worp);
   }
 
 function dobbel() {
     while(dobbelArray.length < 8) {
-        	dobbelArray.push(Math.floor((Math.random() * 6) + 1))
+        let random = Math.floor((Math.random() * 6) + 1);
+        dobbelArray.push(random);
+        worp[random]++;
     }
+
+    tabel();
 }
 
+function tabel() {
+    document.getElementById("a").innerHTML = worp[1];
+    document.getElementById("b").innerHTML = worp[2];
+    document.getElementById("c").innerHTML = worp[3];
+    document.getElementById("d").innerHTML = worp[4];
+    document.getElementById("e").innerHTML = worp[5];
+    document.getElementById("f").innerHTML = worp[6];
+}
